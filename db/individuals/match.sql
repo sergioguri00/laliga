dfeed66121be3def0e2e637a2fc589b8ba072c61
sergioguri00 <guerrerorivero.sergio@gmail.com>
2025-01-02 16:23:37 +0100

    new players and matches

---
+++ db/individuals/match.sql
@@ -0,0 +1,4 @@
+insert into lldb.match (matchdate, localTeam_id, awayTeam_id, stadium_id, matchday_id)
+values  ('2024-08-15', 1, 7, 1, 1),
+        ('2024-08-15', 13, 8, 13, 1),
+        ('2024-08-16', 10, 5, 10, 1);
