7fad8a0032a7cd3f3444ebf2bd505a3b3f63731b
sergioguri00 <guerrerorivero.sergio@gmail.com>
2025-01-22 15:42:50 +0100

    added mainblack as global color def

--- server/tailwind.config.mjs
+++ server/tailwind.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'la-liga-font': ['laligafont', 'sans-serif'], // Aquí defines tu fuente
-			  },
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'la-liga-font': ['laligafont', 'sans-serif']
+      },
+      colors: {
+        mainblack: '#00001B'
+      }
+    }
+  },
+  plugins: []
 }
