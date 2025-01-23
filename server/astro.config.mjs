13c7db4b3ca65a87a16042ccae023e1629104062
sergioguri00 <guerrerorivero.sergio@gmail.com>
2025-01-23 10:14:51 +0100

    added multilanguage :)

--- server/astro.config.mjs
+++ server/astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+    routing: {
+      prefixDefaultLocale: true
+    }
+  },
   integrations: [tailwind()]
-});
+})
