6daeee9c433f209ec79a280fee2ca61f5aa1e540
sergioguri00 <guerrerorivero.sergio@gmail.com>
2025-01-24 14:03:22 +0100

    more stuff

--- db/individuals/03_team.sql
+++ db/individuals/03_team.sql
@@ -1,22 +1,22 @@
-INSERT INTO lldb.team (name,`year`,website,president,badge,city,mainColor,secondaryColor,shortName) VALUES
-	 ('Athletic Club',1898,'https://www.athletic-club.eus/','Jon Uriarte','https://assets.laliga.com/assets/2019/06/07/small/athletic.png','Bilbao','#f21e27','#ffffff','Athletic Club'),
-	 ('Club Atlético de Madrid SAD',1903,'https://www.atleticodemadrid.com/','Enrique Cerezo','https://assets.laliga.com/assets/2024/06/17/small/cbc5c8cc8c3e8abd0e175c00ee53b723.png','Madrid','#f21e27','#ffffff','Atlético de Madrid'),
-	 ('Club Atlético Osasuna',1920,'https://www.osasuna.es/','Luis Sabalza','https://assets.laliga.com/assets/2019/06/07/small/osasuna.png','Pamplona','#bc161c','#091845','CA Osasuna'),
-	 ('Club Deportivo Leganés SAD',1928,'https://www.cdleganes.com/','Jeff Luhnow','https://assets.laliga.com/assets/2019/06/07/small/leganes.png','Leganés','#0f39b8','#ffffff','CD Leganés'),
-	 ('Deportivo Alavés SAD',1921,'https://www.deportivoalaves.com/es/','Alfonso Fernández de Trocóniz','https://assets.laliga.com/assets/2020/09/01/small/27002754a98bf535807fe49a24cc63ea.png','Vitoria-Gasteiz','#0f39b8','#ffffff','Deportivo Alavés'),
-	 ('Fútbol Club Barcelona',1899,'https://www.fcbarcelona.es/es/','Joan Laporta','https://assets.laliga.com/assets/2019/06/07/small/barcelona.png','Barcelona','#0f39b8','#bc161c','FC Barcelona'),
-	 ('Getafe Club de Fútbol SAD',1983,'https://www.getafecf.com/','Ángel Torres','https://assets.laliga.com/assets/2023/05/12/small/dc59645c96bc2c9010341c16dd6d4bfa.png','Getafe','#0f39b8','#0f39b8','Getafe CF'),
-	 ('Girona Fútbol Club SAD',1930,'https://www.gironafc.cat/','Delfí Geli','https://assets.laliga.com/assets/2022/06/22/small/8f43addbb29e4a72f5e90b6edfe4728f.png','Girona','#bc161c','#ffffff','Girona FC'),
-	 ('Rayo Vallecano de Madrid SAD',1924,'https://www.rayovallecano.es/','Raúl Martín','https://assets.laliga.com/assets/2023/04/27/small/57d9950a8745ead226c04d37235c0786.png','Madrid','#ffffff','#f21e27','Rayo Vallecano'),
-	 ('Real Club Celta de Vigo SAD',1923,'https://www.rccelta.es/','Marián Mouriño','https://assets.laliga.com/assets/2019/06/07/small/celta.png','Vigo','#52c3f1','#ffffff','RC Celta');
-INSERT INTO lldb.team (name,`year`,website,president,badge,city,mainColor,secondaryColor,shortName) VALUES
-	 ('RCD Espanyol de Barcelona',1900,'https://www.rcdespanyol.com/','Chen Yansheng','https://assets.laliga.com/assets/2019/06/07/small/espanyol.png','Barcelona','#5a7cf1','#ffffff','RCD Espanyol de Barcelona'),
-	 ('Real Club Deportivo Mallorca SAD',1916,'https://www.rcdmallorca.es/','Andy Kohlberg','https://assets.laliga.com/assets/2023/03/22/small/013ae97735bc8e519dcf30f6826168ca.png','Mallorca','#bc161c','#bc161c','RCD Mallorca'),
-	 ('Real Betis Balompié SAD',1907,'https://www.realbetisbalompie.es/','Ángel Haro','https://assets.laliga.com/assets/2022/09/15/small/e4a09419d3bd115b8f3dab73d480e146.png','Sevilla','#038316','#ffffff','Real Betis'),
-	 ('Real Madrid Club de Fútbol',1902,'https://www.realmadrid.com/','Florentino Pérez','https://assets.laliga.com/assets/2019/06/07/small/real-madrid.png','Madrid','#ffffff','#ffffff','Real Madrid CF'),
-	 ('Real Sociedad de Fútbol SAD',1909,'https://www.realsociedad.eus/','Jokin Aperribay','https://assets.laliga.com/assets/2019/06/07/small/real-sociedad.png','Donostia','#0066ad','#ffffff','Real Sociedad'),
-	 ('Real Valladolid Club de Fútbol SAD',1928,'https://www.realvalladolid.es/','Ronaldo Nazário','https://assets.laliga.com/assets/2024/06/17/small/1467dcd5efb813a742d86f8eb39504a3.png','Valladolid','#63257e','#ffffff','Real Valladolid CF'),
-	 ('Sevilla Fútbol Club SAD',1905,'https://www.sevillafc.es/','José María Del Nido','https://assets.laliga.com/assets/2019/06/07/small/sevilla.png','Sevilla','#ffffff','#f21e27','Sevilla FC'),
-	 ('Unión Deportiva Las Palmas SAD',1949,'https://www.udlaspalmas.es/','Miguel Ángel Ramírez','https://assets.laliga.com/assets/2019/06/07/small/las-palmas.png','Las Palmas de Gran Canaria','#ffd301','#0f39b8','UD Las Palmas'),
-	 ('Valencia Club de Fútbol SAD',1919,'https://www.valenciacf.com/','Layhoon Chan','https://assets.laliga.com/assets/2019/06/07/small/valencia.png','Valencia','#ffffff','#000000','Valencia CF'),
-	 ('Villarreal Club de Fútbol SAD',1923,'https://www.villarrealcf.es/','Fernando Roig','https://assets.laliga.com/assets/2019/06/07/small/villarreal.png','Villarreal','#ffd301','#ffd301','Villarreal CF');
+INSERT INTO lldb.team (name,shortName,`year`,website,president,badge,city,mainColor,secondaryColor) VALUES
+	 ('Athletic Club','Athletic Club',1898,'https://www.athletic-club.eus/','Jon Uriarte','https://assets.laliga.com/assets/2019/06/07/small/athletic.png','Bilbao','#f21e27','#ffffff'),
+	 ('Club Atlético de Madrid SAD','Atlético de Madrid',1903,'https://www.atleticodemadrid.com/','Enrique Cerezo','https://assets.laliga.com/assets/2024/06/17/small/cbc5c8cc8c3e8abd0e175c00ee53b723.png','Madrid','#f21e27','#ffffff'),
+	 ('Club Atlético Osasuna','CA Osasuna',1920,'https://www.osasuna.es/','Luis Sabalza','https://assets.laliga.com/assets/2019/06/07/small/osasuna.png','Pamplona','#bc161c','#091845'),
+	 ('Club Deportivo Leganés SAD','CD Leganés',1928,'https://www.cdleganes.com/','Jeff Luhnow','https://assets.laliga.com/assets/2019/06/07/small/leganes.png','Leganés','#0f39b8','#ffffff'),
+	 ('Deportivo Alavés SAD','Deportivo Alavés',1921,'https://www.deportivoalaves.com/es/','Alfonso Fernández de Trocóniz','https://assets.laliga.com/assets/2020/09/01/small/27002754a98bf535807fe49a24cc63ea.png','Vitoria-Gasteiz','#0f39b8','#ffffff'),
+	 ('Fútbol Club Barcelona','FC Barcelona',1899,'https://www.fcbarcelona.es/es/','Joan Laporta','https://assets.laliga.com/assets/2019/06/07/small/barcelona.png','Barcelona','#0f39b8','#bc161c'),
+	 ('Getafe Club de Fútbol SAD','Getafe CF',1983,'https://www.getafecf.com/','Ángel Torres','https://assets.laliga.com/assets/2023/05/12/small/dc59645c96bc2c9010341c16dd6d4bfa.png','Getafe','#0f39b8','#0f39b8'),
+	 ('Girona Fútbol Club SAD','Girona FC',1930,'https://www.gironafc.cat/','Delfí Geli','https://assets.laliga.com/assets/2022/06/22/small/8f43addbb29e4a72f5e90b6edfe4728f.png','Girona','#bc161c','#ffffff'),
+	 ('Rayo Vallecano de Madrid SAD','Rayo Vallecano',1924,'https://www.rayovallecano.es/','Raúl Martín','https://assets.laliga.com/assets/2023/04/27/small/57d9950a8745ead226c04d37235c0786.png','Madrid','#ffffff','#f21e27'),
+	 ('Real Club Celta de Vigo SAD','RC Celta',1923,'https://www.rccelta.es/','Marián Mouriño','https://assets.laliga.com/assets/2019/06/07/small/celta.png','Vigo','#52c3f1','#ffffff');
+INSERT INTO lldb.team (name,shortName,`year`,website,president,badge,city,mainColor,secondaryColor) VALUES
+	 ('RCD Espanyol de Barcelona','RCD Espanyol',1900,'https://www.rcdespanyol.com/','Chen Yansheng','https://assets.laliga.com/assets/2019/06/07/small/espanyol.png','Barcelona','#5a7cf1','#ffffff'),
+	 ('Real Club Deportivo Mallorca SAD','RCD Mallorca',1916,'https://www.rcdmallorca.es/','Andy Kohlberg','https://assets.laliga.com/assets/2023/03/22/small/013ae97735bc8e519dcf30f6826168ca.png','Mallorca','#bc161c','#bc161c'),
+	 ('Real Betis Balompié SAD','Real Betis',1907,'https://www.realbetisbalompie.es/','Ángel Haro','https://assets.laliga.com/assets/2022/09/15/small/e4a09419d3bd115b8f3dab73d480e146.png','Sevilla','#038316','#ffffff'),
+	 ('Real Madrid Club de Fútbol','Real Madrid CF',1902,'https://www.realmadrid.com/','Florentino Pérez','https://assets.laliga.com/assets/2019/06/07/small/real-madrid.png','Madrid','#ffffff','#ffffff'),
+	 ('Real Sociedad de Fútbol SAD','Real Sociedad',1909,'https://www.realsociedad.eus/','Jokin Aperribay','https://assets.laliga.com/assets/2019/06/07/small/real-sociedad.png','Donostia','#0066ad','#ffffff'),
+	 ('Real Valladolid Club de Fútbol SAD','Real Valladolid CF',1928,'https://www.realvalladolid.es/','Ronaldo Nazário','https://assets.laliga.com/assets/2024/06/17/small/1467dcd5efb813a742d86f8eb39504a3.png','Valladolid','#63257e','#ffffff'),
+	 ('Sevilla Fútbol Club SAD','Sevilla FC',1905,'https://www.sevillafc.es/','José María Del Nido','https://assets.laliga.com/assets/2019/06/07/small/sevilla.png','Sevilla','#ffffff','#f21e27'),
+	 ('Unión Deportiva Las Palmas SAD','UD Las Palmas',1949,'https://www.udlaspalmas.es/','Miguel Ángel Ramírez','https://assets.laliga.com/assets/2019/06/07/small/las-palmas.png','Las Palmas de Gran Canaria','#ffd301','#0f39b8'),
+	 ('Valencia Club de Fútbol SAD','Valencia CF',1919,'https://www.valenciacf.com/','Layhoon Chan','https://assets.laliga.com/assets/2019/06/07/small/valencia.png','Valencia','#ffffff','#000000'),
+	 ('Villarreal Club de Fútbol SAD','Villarreal CF',1923,'https://www.villarrealcf.es/','Fernando Roig','https://assets.laliga.com/assets/2019/06/07/small/villarreal.png','Villarreal','#ffd301','#ffd301');
