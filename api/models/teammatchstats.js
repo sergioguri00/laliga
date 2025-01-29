4d74296bfe5499ee931b273bde6d81e45bf0f8d9
sergioguri00 <guerrerorivero.sergio@gmail.com>
2025-01-29 10:47:42 +0100

    updated api to accept both team match stats

--- api/models/teammatchstats.js
+++ api/models/teammatchstats.js
@@ -49,39 +49,50 @@ export class TeamMatchStatsModel {
 
   static async create ({ input }) {
     const {
-      team,
       matchday,
-      possession,
-      shots,
-      shotsOnTarget,
-      corners,
-      offsides,
-      fouls,
-      goals
+      localTeam,
+      localPossession,
+      localShots,
+      localShotsOnTarget,
+      localCorners,
+      localOffsides,
+      localFouls,
+      localGoals,
+      awayTeam,
+      awayPossession,
+      awayShots,
+      awayShotsOnTarget,
+      awayCorners,
+      awayOffsides,
+      awayFouls,
+      awayGoals
     } = input
     try {
-      const [checkIfMatchExists] = await connection.execute('SELECT * FROM `match` WHERE (localTeam_id = ? OR awayTeam_id = ?) AND matchday_id = ?', [team, team, matchday])
+      const [checkIfMatchExists] = await connection.execute('SELECT * FROM `match` WHERE (localTeam_id = ? AND awayTeam_id = ?) AND matchday_id = ?', [localTeam, awayTeam, matchday])
       if (checkIfMatchExists.length === 1) {
         await connection.query(
           `INSERT INTO teammatchstats(team_id, matchday_id, possession, shots, shots_on_target, corners, offsides, fouls, goals)
           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-          [team, matchday, possession, shots, shotsOnTarget, corners, offsides, fouls, goals]
+          [localTeam, matchday, localPossession, localShots, localShotsOnTarget, localCorners, localOffsides, localFouls, localGoals]
         )
-        const [checkIfMatchIsCompleted] = await connection.execute('SELECT * FROM teammatchstats WHERE matchday_id = ? AND (team_id = ? OR team_id = ?)', [matchday, checkIfMatchExists[0].localTeam_id, checkIfMatchExists[0].awayTeam_id])
-        if (checkIfMatchIsCompleted.length === 2) {
-          if (checkIfMatchIsCompleted[0].goals > checkIfMatchIsCompleted[1].goals) {
-            await connection.query('UPDATE teamstats SET points = points + 3, matches_played = matches_played + 1, wins = wins + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [checkIfMatchIsCompleted[0].goals, checkIfMatchIsCompleted[1].goals, checkIfMatchIsCompleted[0].team_id])
-            await connection.query('UPDATE teamstats SET matches_played = matches_played + 1, losses = losses + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [checkIfMatchIsCompleted[1].goals, checkIfMatchIsCompleted[0].goals, checkIfMatchIsCompleted[1].team_id])
-          } else if (checkIfMatchIsCompleted[0].goals < checkIfMatchIsCompleted[1].goals) {
-            await connection.query('UPDATE teamstats SET points = points + 3, matches_played = matches_played + 1, wins = wins + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [checkIfMatchIsCompleted[1].goals, checkIfMatchIsCompleted[0].goals, checkIfMatchIsCompleted[1].team_id])
-            await connection.query('UPDATE teamstats SET matches_played = matches_played + 1, losses = losses + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [checkIfMatchIsCompleted[0].goals, checkIfMatchIsCompleted[1].goals, checkIfMatchIsCompleted[0].team_id])
-          } else if (checkIfMatchIsCompleted[0].goals === checkIfMatchIsCompleted[1].goals) {
-            await connection.query('UPDATE teamstats SET points = points + 1, matches_played = matches_played + 1, draws = draws + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [checkIfMatchIsCompleted[0].goals, checkIfMatchIsCompleted[1].goals, checkIfMatchIsCompleted[0].team_id])
-            await connection.query('UPDATE teamstats SET points = points + 1, matches_played = matches_played + 1, draws = draws + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [checkIfMatchIsCompleted[1].goals, checkIfMatchIsCompleted[0].goals, checkIfMatchIsCompleted[1].team_id])
-          }
+        await connection.query(
+          `INSERT INTO teammatchstats(team_id, matchday_id, possession, shots, shots_on_target, corners, offsides, fouls, goals)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+          [awayTeam, matchday, awayPossession, awayShots, awayShotsOnTarget, awayCorners, awayOffsides, awayFouls, awayGoals]
+        )
+        if (localGoals > awayGoals) {
+          await connection.query('UPDATE teamstats SET points = points + 3, matches_played = matches_played + 1, wins = wins + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [localGoals, awayGoals, localTeam])
+          await connection.query('UPDATE teamstats SET matches_played = matches_played + 1, losses = losses + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [awayGoals, localGoals, awayTeam])
+        } else if (localGoals < awayGoals) {
+          await connection.query('UPDATE teamstats SET points = points + 3, matches_played = matches_played + 1, wins = wins + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [awayGoals, localGoals, awayTeam])
+          await connection.query('UPDATE teamstats SET matches_played = matches_played + 1, losses = losses + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [localGoals, awayGoals, localGoals])
+        } else if (localGoals === awayGoals) {
+          await connection.query('UPDATE teamstats SET points = points + 1, matches_played = matches_played + 1, draws = draws + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [localGoals, awayGoals, localTeam])
+          await connection.query('UPDATE teamstats SET points = points + 1, matches_played = matches_played + 1, draws = draws + 1, goals_scored = goals_scored + ?, goals_conceded = goals_conceded + ?  WHERE team_id = ?', [awayGoals, localGoals, awayTeam])
         }
-        const [newTeamMatchStats] = await connection.execute('SELECT * FROM teammatchstats WHERE team_id = ? AND matchday_id = ?', [team, matchday])
-        return newTeamMatchStats
+        const [newLocalTeamMatchStats] = await connection.execute('SELECT * FROM teammatchstats WHERE team_id = ? AND matchday_id = ?', [localTeam, matchday])
+        const [newAwayTeamMatchStats] = await connection.execute('SELECT * FROM teammatchstats WHERE team_id = ? AND matchday_id = ?', [awayTeam, matchday])
+        return { localTeamMatchStats: newLocalTeamMatchStats[0], awayTeamMatchStats: newAwayTeamMatchStats[0] }
       } else {
         return { error: 'Match does not exist' }
       }
--- api/schemas/teammatchstats.js
+++ api/schemas/teammatchstats.js
@@ -1,15 +1,23 @@
 import { z } from 'zod'
 
 const teamMatchStatsSchema = z.object({
-  team: z.number().int().min(0),
   matchday: z.number().int().min(1),
-  possession: z.number().int().min(0),
-  shots: z.number().int().min(0).nullable(),
-  shotsOnTarget: z.number().int().min(0).nullable(),
-  corners: z.number().int().min(0).nullable(),
-  offsides: z.number().int().min(0).nullable(),
-  fouls: z.number().int().min(0).nullable(),
-  goals: z.number().int().min(0)
+  localTeam: z.number().int().min(0),
+  localPossession: z.number().int().min(0),
+  localShots: z.number().int().min(0).nullable(),
+  localShotsOnTarget: z.number().int().min(0).nullable(),
+  localCorners: z.number().int().min(0).nullable(),
+  localOffsides: z.number().int().min(0).nullable(),
+  localFouls: z.number().int().min(0).nullable(),
+  localGoals: z.number().int().min(0),
+  awayTeam: z.number().int().min(0),
+  awayPossession: z.number().int().min(0),
+  awayShots: z.number().int().min(0).nullable(),
+  awayShotsOnTarget: z.number().int().min(0).nullable(),
+  awayCorners: z.number().int().min(0).nullable(),
+  awayOffsides: z.number().int().min(0).nullable(),
+  awayFouls: z.number().int().min(0).nullable(),
+  awayGoals: z.number().int().min(0)
 })
 
 export function validateTeamMatchStats (input) {
